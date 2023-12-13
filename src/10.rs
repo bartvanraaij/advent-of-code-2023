27b41705bd7960e4bfd75cbd9afa117e9d9bbfa3
Bart van Raaij <info@bartvanraaij.nl>
2023-12-13 15:15:55 +0100

    Scaffold day 10

--- src/10.rs
+++ src/10.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use std::{env, fs};
 
 fn read_input_file(args: Vec<String>) -> String {
-    let default_input_filename = &String::from("input/00");
+    let default_input_filename = &String::from("input/10");
     let input_filepath: &str = args.get(1).unwrap_or(default_input_filename);
     fs::read_to_string(input_filepath).expect("input file should be readable")
 }
@@ -19,6 +19,7 @@ fn main() {
 fn part_1(input: &str) -> usize {
     //input.split("\n").filter(|l| !l.is_empty()).map(|line| {});
 
+
     0
 }
 
@@ -27,19 +28,50 @@ fn part_2(input: &str) -> usize {
 }
 
 #[cfg(test)]
-mod tests_00 {
+mod tests_10 {
     use super::*;
 
-    const SAMPLE_DATA: &str = r#"
+    const SAMPLE_DATA_1: &str = r#"
+.....
+.S-7.
+.|.|.
+.L-J.
+.....
 "#;
 
+  const SAMPLE_DATA_2: &str = r#"
+-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
+"#;
+
+    const SAMPLE_DATA_3: &str = r#"
+..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+"#;
+
+    const SAMPLE_DATA_4: &str = r#"
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+"#;
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(SAMPLE_DATA), 0);
+        assert_eq!(part_1(SAMPLE_DATA_1), 4);
+        assert_eq!(part_1(SAMPLE_DATA_2), 4);
+        assert_eq!(part_1(SAMPLE_DATA_3), 8);
+        assert_eq!(part_1(SAMPLE_DATA_4), 8);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(SAMPLE_DATA), 0);
+        //assert_eq!(part_2(SAMPLE_DATA), 0);
     }
 }
